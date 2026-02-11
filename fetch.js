d990341a01309c19fc2aaab2f9760b095d479a45
wbto1 <bosmanswim@gmail.com>
2026-02-11 13:04:04 +0100

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+
+const URL = 'https://api.tvmaze.com/schedule?country=BE';
+const today = new Date().toISOString().split('T')[0];
+
+async function run() {
+  const res = await fetch(URL);
+  const schedule = await res.json();
+
+  const premieres = schedule
+    .filter(item =>
+      item.show &&
+      item.show.type === 'Scripted' &&
+      item.show.premiered === today
+    )
+    .map(item => ({
+      title: item.show.name,
+      channel: item.show.network?.name || 'Onbekend',
+      from: item.show.webChannel?.name || 'Onbekend'
+    }));
+
+  const log = JSON.parse(fs.readFileSync('log.json', 'utf8'));
+  log[today] = premieres;
+
+  fs.writeFileSync('log.json', JSON.stringify(log, null, 2));
+}
+
+run();
